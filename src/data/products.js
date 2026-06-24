64db5b85fe92e39446e11564c221fa3ca1805963
nasser <nasser.ahmedg20@gmail.com>
2026-06-24 04:10:25 +0100

    fix product page

--- src/data/products.js
+++ src/data/products.js
@@ -14,61 +14,61 @@ export const products = [{
     name : 'blue-shirt',
     id:1,
     img : tblue1,
-    desc: 't-shirt black big size for fat bitch',
-    price : 100
+    desc: 't-shirt black big size for man',
+   price : 100
    },{
     name : 'black-shirt',
     id:2,
     img : tblack2,
-    desc: 't-shirt black big size for fat bitch',
-    price : 200
+    desc: 't-shirt black big size for man',
+   price : 200
    },{
     name : 'zara-shirt',
     id:3,
     img : tblue2,
-    desc: 't-shirt black big size for fat bitch',
+    desc: 't-shirt black big size for man',
     price : 300
    },{
     name : 'addidas-shirt',
     id:4,
     img : tblue3,
-    desc: 't-shirt black big size for fat bitch',
+    desc: 't-shirt black big size for man',
     price : 340
    },{
     name : 'puma-shirt',
     id:5,
     img : tgrey,
-    desc: 't-shirt black big size for fat bitch',
+    desc: 't-shirt black big size for man',
     price : 40
    },{
     name : 'white-shirt',
     id:6,
     img : tblue1,
-    desc: 't-shirt black big size for fat bitch',
+    desc: 't-shirt black big size for man',
     price : 230
    },{
     name : 'black-shirt',
     id:7,
     img : tblue2,
-    desc: 't-shirt black big size for fat bitch',
+    desc: 't-shirt black big size for man',
     price : 400
 
    },{
     name : 'bershka-shirt',
     id:8,
     img : tblack2,
-    desc: 't-shirt black big size for fat bitch',
+    desc: 't-shirt black big size for man',
     price : 70
    },{
     name : 'man-shirt',
     id:9,
     img : tblue3,
-    desc: 't-shirt black big size for fat bitch',
+    desc: 't-shirt black big size for man',
     price : 600
    },{
     name : 'baby-shirt',
     id:10,
     img : tgrey,
-    desc: 't-shirt black big size for fat bitch',
+    desc: 't-shirt black big size for man',
     price : 170
    }];
--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -2,7 +2,6 @@ import { useParams } from "react-router-dom";
 import { products } from "../data/products";
 import { useContext } from "react";
 import CartContext from "../components/cartcontext";
-import { Link } from "react-router-dom";
 
 
 export default function ProductDetails({opensidebar}) {
@@ -14,43 +13,50 @@ export default function ProductDetails({opensidebar}) {
     (product) => product.id === Number(id)
   );
  const {
-  addtocart,
+        cart,
+        addtocart,
+        increaseQuantity,
+        decreaseQuantity,
 } = useContext(CartContext);
   if (!product) {
     return <h2>Product not found</h2>;
   }
-
+  const quantity = cart.find(item => item.id === Number(id))?.quantity || 0;
    
   return (
     <>
-      <section className={`pt-[200px] px-6 flex gap-8  min-h-screen flex-col w-full ${opensidebar? "ml-[300px]" : "ml-[60px]"} transition-all duration-300`}>
-        <div className="border rounded-xl p-4 h-fit flex w-fit items-center ml-20">
-        <div>
+      <section className={`pt-[60px]  flex   min-h-screen flex-col  ${opensidebar? "ml-[300px]" : "ml-[60px]"} transition-all duration-300`}>
+        <div className=" flex w-full items-center  justify-center flex-1 max-md:flex-col">
+        <div className="w-[50%] md:w-1/2 justify-center flex">
           <img
             src={product.img}
             alt={product.name}
-            className="w-[400px]"
+            className="w-[600px] h-[400px] object-contain p-4"
           />
         </div>
 
-        <div className="flex flex-col gap-4 ">
-          <h1 className="text-3xl font-bold">
+        <div className="flex flex-col gap-4 w-[50%] md:w-1/2 ">
+          <h1 className="text-6xl max-md:text-xl">
             {product.name}
           </h1>
 
-          <p>{product.desc}</p>
+          <p className="text-2xl mt-6 max-md:text-xl">{product.desc}</p>
 
-          <p className="font-bold text-xl">
+          <p className="font-bold text-3xl mt-4 max-md:text-xl">
             ${product.price}
           </p>
-            <div className="w-full flex  gap-4  justify-end">
-              <Link to="/checkout">
-                <button className="text-white bg-black p-4 rounded-xl hover:bg-yellow-800 hover:text-black transition-all cursor-pointer">checkout</button>
-              </Link>
-          <button className="px-3 py-2 text-white bg-yellow-800 font-bold rounded-xl cursor-pointer hover:bg-black hover:text-white transition-all" onClick={() => {addtocart(product)}}>add to cart</button>
-        </div>
-        </div>
-        </div>
+          </div>
+          </div>
+            <div className="flex justify-end gap-4 items-center m-4">
+              {quantity > 0 && ( 
+                <>
+              <button className="text-xl cursor-pointer" onClick={() => {decreaseQuantity(product.id)}}>-</button>
+              <span>{quantity}</span>
+              <button className="cursor-pointer" onClick={() => {increaseQuantity(product.id)}}>+</button>
+              </>
+              )}
+              <button className="text-white rounded-xl font-bold px-4 py-2 hover:bg-red-400 bg-red-700 cursor-pointer" onClick={() => {addtocart(product)}}>add to cart</button>
+            </div>
       </section>
     </>
   );
